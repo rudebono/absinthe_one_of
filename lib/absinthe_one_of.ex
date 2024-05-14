a5bf595f2ce6eb3b4c8b28ea699284f12829773f
Hany Rock <rudebono@gmail.com>
2024-05-14 16:57:34 +0900

    init

---
+++ lib/absinthe_one_of.ex
@@ -0,0 +1,18 @@
+defmodule AbsintheOneOf do
+  @moduledoc """
+  Documentation for `AbsintheOneOf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AbsintheOneOf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/absinthe_one_of_test.exs
@@ -0,0 +1,8 @@
+defmodule AbsintheOneOfTest do
+  use ExUnit.Case
+  doctest AbsintheOneOf
+
+  test "greets the world" do
+    assert AbsintheOneOf.hello() == :world
+  end
+end
