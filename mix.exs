b7032ff1881fc652c931c050cafcec8765669eb2
Hany Rock <rudebono@gmail.com>
2024-05-15 01:01:57 +0900

    version bump to "1.1.0"

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheOneOf.MixProject do
   def project() do
     [
       app: :absinthe_one_of,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
